d767ab92a5480e2dcf7b8f9294abd545ea43040b
LUISREYESELTRENZAS <124694031+LUISREYESELTRENZAS@users.noreply.github.com>
2024-02-20 06:08:23 +0000

    📓Scripts para distribución

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,54 @@
-// Notas importanes
-// El archivo de configuración debe usar ES5
-
+// es por ello que veras "requires" no "imports"
 // Importar un administrador de rutas de archivos
 const path = require('path');
-
 // Exportamos un objeto de configuración
 // que sera usado por webpack
 module.exports = {
-  ...
+  // 0. Modo de empaquetado
+  mode: "production",
+  // 1. El archivo de entrada a partir del cual
+  // contendra todas las definiciones a empaquetar
+  entry: "./client/index.js",
+  // 2. Especificar el archivo de salida
+  // Aqui se detalla donde quedara el archivo
+  // final empaquetado.
   output: {
-    // 2.1 Absolute output path
-    // Note that it is being placed in the directory
-    // of the project's static files
+    // 2.1 Ruta absoluta de salida
+    // Note que se esta colocando en el directorio
+    // de archivos estáticos del proyecto
     path: path.resolve(__dirname, "public"),
-    // 2.2 Output file name
-    filename: "bundle.js"
+    // 2.2 Nombre del archivo de salida
+    filename: "bundle.js",
+      // 2.3 Ruta base de archivos estáticos
+		publicPath: "/"
   },
-  // Agregando un modulo a webpack
-  module: {
+   // 3. Configuración de los loaders
+   module:{
     rules: [
+      // 3.1 Reglas para archivos JS
       {
+        // 3.1.1 Expresión regular para identificar archivos
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
+        // 3.1.2 Excluir archivos de la carpeta node_modules
+        exclude: /node_modules/,
+        // 3.1.3 Usar el loader de babel
+        use:[
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
+            // 3.1.4 Opciones de configuración de babel
             options: {
               presets: [
                 [
-                  '@babel/preset-env',
+                  "@babel/preset-env",
+                  // 3.1.5 Opciones de configuración de preset-env 
                   {
-                    'modules': false,
-                    'useBuiltIns': 'usage',
-                    'targets': '> 0.25%, not dead',
-                    'corejs': 3
+                    "modules": false,
+                    "useBuiltIns": "usage",
+                    // 3.1.6 Corejs para usar con polyfills
+                    "targets": '> 0.25%, not dead',
+                    "corejs": 3
                   }
-                ]
+                ],
               ]
             }
           }
@@ -43,4 +56,4 @@ module.exports = {
       }
     ]
   }
-}
+};
