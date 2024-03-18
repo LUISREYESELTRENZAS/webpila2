e58ce8c38329f0f4169becf7e4287095c79095b6
LUISREYESELTRENZAS <124694031+LUISREYESELTRENZAS@users.noreply.github.com>
2024-03-18 01:38:12 +0000

    📖Guía de estilos de AirBnB📚

--- client/index.js
+++ client/index.js
@@ -1,29 +1,4 @@
 // cargando los estilos
-import './styles/style.css'
-console.log("🎉 Client Server Working powered by webpack 🎉");
+import './styles/style.css';
 
-// Codigo ES6
-// Default Parameters
-let show = (msg="No message given") => {
-  console.log(msg)
-}
-
-// Async Await
-function resolveAfter2Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000)
-  });
-}
-
-// Funcion asincrona
-async function asyncCall() {
-  console.log("Calling");
-  const result = await resolveAfter2Seconds();
-  console.log(result);
-}
-
-// Llmar a la funcion asincrona
-asyncCall();
-show();
+console.log('🎉 Client Server Working powered by webpack 🎉');
