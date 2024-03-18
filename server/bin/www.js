8604b54c30179b6a7f510c925116cae60ac814e8
LUISREYESELTRENZAS <124694031+LUISREYESELTRENZAS@users.noreply.github.com>
2024-03-18 00:47:11 +0000

    🎉Webpack middleware 🌐

--- server/bin/www.js
+++ server/bin/www.js
@@ -7,7 +7,7 @@
 import app from '../app';
 import debugLib from 'debug';
 import http from 'http';
-const debug = debugLib('expweb-bp:server'); 
+const debug = debugLib('it-server'); 
 /**
  * Get port from environment and store in Express.
  */
