e216ce497e2d655de88eea5f7ef794a93d4b6102
LUISREYESELTRENZAS <124694031+LUISREYESELTRENZAS@users.noreply.github.com>
2024-03-26 19:34:28 +0000

    🌟 Rutas de Acciones corregido

--- server/domains/user/user.controller.js
+++ server/domains/user/user.controller.js
@@ -1,18 +1,23 @@
-// Action Methods
-
-// GET /user/login
+// Actions methods
+// GET "/user/login"
 const login = (req, res) => {
-    res.send('⚠️ UNDER CONSTRUCTION: GET /user/login ⚠️');
-  };
-  
-  // GET "/user/logout"
-  const logout = (req, res) => {
-    res.send('⚠️ UNDER CONSTRUCTION: GET /user/logout ⚠️');
-  };
-  
-  // GET "/user/register"
-  const register = (req, res) => {
-    res.send('⚠️ UNDER CONSTRUCTION: GET /user/register ⚠️');
-  };
-  
-  export default { login, logout, register };
+  res.send('⚠️ UNDER CONSTRUCTION: GET /user/login ⚠️');
+};
+
+// GET "/user/logout"
+const logout = (req, res) => {
+  res.send('⚠️ UNDER CONSTRUCTION: GET /user/logout ⚠️');
+};
+
+// GET "/user/register"
+const register = (req, res) => {
+  res.send('⚠️ UNDER CONSTRUCTION: GET /user/register ⚠️');
+};
+
+// Controlador user
+export default {
+  // Action Methods
+  login,
+  logout,
+  register,
+};
